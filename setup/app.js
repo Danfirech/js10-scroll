de9fcb386ec52fdea404cc60675a6f0a80119cc9
Dan Possehl <danielbpossehl@gmail.com>
2021-12-07 10:24:54 -0700

    set date

--- setup/app.js
+++ setup/app.js
@@ -5,6 +5,10 @@
 
 // ********** set date ************
 
+const date = document.getElementById("date");
+
+date.innerHTML = new Date().getFullYear();
+
 // ********** close links ************
 
 // ********** fixed navbar ************
