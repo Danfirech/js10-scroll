4c5151b6e960bcbbdc05c888d93d8320b60f5e66
Dan Possehl <danielbpossehl@gmail.com>
2021-12-07 11:43:53 -0700

    added -navheight function

--- setup/app.js
+++ setup/app.js
@@ -61,7 +61,13 @@ scrollLinks.forEach(function (link) {
     // navigate to specific spot
     const id = e.currentTarget.getAttribute("href").slice(1);
     const element = document.getElementById(id);
-    let position = element.offsetTop;
+
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+
+    let position = element.offsetTop - navHeight;
     console.log(position);
     window.scrollTo({ left: 0, top: position });
     linksContainer.style.height = 0;
